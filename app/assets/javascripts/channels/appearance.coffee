ad382e95dc137e86b87d7f506fb173ba4fb565e6
kkrasilov <kkrasilov@gmail.com>
2020-11-23 18:50:27 +0500

    refactoring code

--- app/assets/javascripts/channels/appearance.coffee
+++ app/assets/javascripts/channels/appearance.coffee
@@ -6,10 +6,10 @@ App.appearance = App.cable.subscriptions.create "AppearanceChannel",
     # Called when the subscription has been terminated by the server
 
   received: (data) ->
-    current_user = data.users
+    current_user = data.user
     element_user = $("#online").find("[data-id=#{current_user.id}]")
 
     if current_user.online && element_user.length == 0
-      $("#online").append(data.html)
-    else if current_user.online == false && element_user.length > 0
+      $("#online").append("<li data-id=#{current_user.id}>#{current_user.nickname}</li>")
+    else if !current_user.online && element_user.length > 0
       element_user.remove()
--- app/services/online_service.rb
+++ app/services/online_service.rb
@@ -6,20 +6,22 @@ class OnlineService
   def make_online!
     @current_user.update!(online: true)
 
-    html = ApplicationController.render(partial: "users/user", locals: { user: @current_user } )
-
-    ActionCable.server.broadcast "appearance_channel", users: @current_user, html: html
+    broadcast_appearance
   end
 
   def make_offline!
     if count_unique_connections.zero?
       @current_user.update!(online: false)
 
-      ActionCable.server.broadcast "appearance_channel", users: @current_user
+      broadcast_appearance
     end
   end
 
   private
+
+  def broadcast_appearance
+    ActionCable.server.broadcast "appearance_channel", user: @current_user
+  end
 
   def count_unique_connections
     ActionCable.server.connections.count do |connection|
