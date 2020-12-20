9e4db465ba5425f16c838f98282f889e6bebdfb6
kkrasilov <kkrasilov@gmail.com>
2020-12-21 01:56:04 +0500

    added views room users

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,6 +4,7 @@ class Room < ApplicationRecord
   belongs_to :admin, class_name: 'User'
   has_many :messages, dependent: :destroy
   has_many :room_users, dependent: :destroy
+  has_many :users, through: :room_users
 
   def to_param
     token
